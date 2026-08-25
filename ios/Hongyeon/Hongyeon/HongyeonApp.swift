6fda3fde5ef58256fcf998127809fc26fe1c1251
Sangha Jeon <sangha082214@gmail.com>
2026-08-25 23:20:41 +0900

    Add iOS project skeleton with location permission prompt
    Closes #9

---
+++ ios/Hongyeon/Hongyeon/HongyeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongyeonApp.swift
+//  Hongyeon
+//
+//  Created by Sangha Jeon on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct HongyeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
