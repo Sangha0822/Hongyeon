96ecba77cbe3f6c0efaf170d672b6af44fc87db7
Sangha Jeon <sangha082214@gmail.com>
2026-09-01 00:04:46 +0900

    Log timestamp and app state on remote notification receipt
    Closes #14

--- ios/Hongyeon/Hongyeon/AppDelegate.swift
+++ ios/Hongyeon/Hongyeon/AppDelegate.swift
@@ -23,7 +23,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        print("Received remote notification: \(userInfo)")
+        print("Received remote notification at \(Date()), app state: \(application.applicationState.rawValue)")
         completionHandler(.newData)
     }
+    
 }
